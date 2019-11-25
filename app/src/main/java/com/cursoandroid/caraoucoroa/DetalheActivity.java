22fc33fa31223dd7569dfc2a0f33f00e81f1f2c4
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-25 15:28:32 -0300

    Adding "resultado" imageview reference.

--- app/src/main/java/com/cursoandroid/caraoucoroa/DetalheActivity.java
+++ app/src/main/java/com/cursoandroid/caraoucoroa/DetalheActivity.java
@@ -3,12 +3,18 @@ package com.cursoandroid.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DetalheActivity extends AppCompatActivity {
+
+    private ImageView resultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe);
+
+        resultado = findViewById(R.id.resultado);
     }
 }
