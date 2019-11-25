411888021497551ed9005282113f837aede91852
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-25 15:19:29 -0300

    Adding "jogar" button references and event listener.

--- app/src/main/java/com/cursoandroid/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/cursoandroid/caraoucoroa/MainActivity.java
@@ -2,13 +2,29 @@ package com.cursoandroid.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView btn_jogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_jogar = findViewById(R.id.btn_jogar);
+
+        btn_jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
