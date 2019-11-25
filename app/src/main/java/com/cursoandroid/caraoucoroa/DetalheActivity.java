e59fd4b23c3cb894b6c3303170e1b143458d837f
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-25 15:30:43 -0300

    Adding "voltar" button reference.

--- app/src/main/java/com/cursoandroid/caraoucoroa/DetalheActivity.java
+++ app/src/main/java/com/cursoandroid/caraoucoroa/DetalheActivity.java
@@ -3,12 +3,13 @@ package com.cursoandroid.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class DetalheActivity extends AppCompatActivity {
 
     private ImageView resultado;
-
+    private ImageView btn_voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +17,15 @@ public class DetalheActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detalhe);
 
         resultado = findViewById(R.id.resultado);
+
+        btn_voltar = findViewById(R.id.btn_voltar);
+
+        btn_voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
